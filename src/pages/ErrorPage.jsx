68f4f88d7394e740c582bf6796cb85e5523a6dc6
steven-solis <kp07761@gmail.com>
2023-02-24 19:35:16 -0600

    Tailwind added

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -3,7 +3,7 @@ export default function ErrorPage() {
 
   return (
     <div id="error-page">
-      <h1>Oops!</h1>
+      <h1 className="">Oops!</h1>
       <p>Lo sentimos, un error inesperado a ocurrido.</p>
       <p>
         Pagina no encontrada
