c09be377032f68563ab1eee4f6fb53af9f3e45a5
AndrikSJD <110579197+AndrikSJD@users.noreply.github.com>
2023-02-26 01:59:48 -0600

    search input element

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,7 +18,14 @@ export function Home() {
     return (
         <>
             <div className='  w-full bg-cover bg-top  p-60'
-                style={{backgroundImage: `url(${mainBanner})`}}>
+                style={{ backgroundImage: `url(${mainBanner})` }}>
+                <div className="flex flex-col justify-center items-center h-full">
+                    <input
+                        className="w-1/2 py-2 px-4 rounded-lg bg-white text-gray-800 font-semibold shadow-md focus:outline-none focus:shadow-outline text-center"
+                        type="text"
+                        placeholder={`"Rick Sanchez"`}
+                    />
+                </div>
                 <button className="absolute top-0 right-0 bg-black rounded-lg btn-transparent text-white text-3xl py-4 px-4 mt-4 mr-4 bg-opacity-50 " style={{ fontFamily: "Oswald, sans-serif" }}
                     onClick={onLogOut}>
                     Cerrar Sesion
